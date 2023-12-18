62d0560fe00b5cedf601db849f4eb09c39538815
AhmerAli17 <ahmerali1771@gmail.com>
2023-12-18 13:39:45 +0000

    first commit

---
+++ src/app/components/clock/clock.tsx
@@ -0,0 +1,26 @@
+"use client"
+import React, { useState, useEffect } from 'react';
+
+const Clock = () => {
+  const [currentTime, setCurrentTime] = useState(new Date());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000);
+
+    // Cleanup interval on component unmount
+    return () => clearInterval(intervalId);
+  }, []); // Pass an empty dependency array to run the effect only once on mount
+
+  const formattedTime = currentTime.toLocaleTimeString([], {
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+  });
+
+  return <div className="digital-clock">{formattedTime}</div>;
+};
+
+export default Clock;
+
---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Clock from './components/clock/clock'
+
+const page = () => {
+  return (
+    <div>
+      <h1 className='flex justify-center items-center '>
+        <Clock/>
+      </h1>
+    </div>
+  )
+}
+
+export default page
