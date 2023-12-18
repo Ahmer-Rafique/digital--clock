8fd2c453113a9aaff26da3018fdbce6755bcaab5
AhmerAli17 <ahmerali1771@gmail.com>
2023-12-18 13:53:11 +0000

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ import Clock from './components/clock/clock'
 const page = () => {
   return (
     <div>
-      <h1 className='flex justify-center items-center '>
-        <Clock/>
+      <h1 className='flex justify-center items-center h-96 text-5xl text-blue-700   '>
+       <div className='bg-red-400 px-7 py-7 rounded-full'><Clock/></div> 
       </h1>
     </div>
   )
